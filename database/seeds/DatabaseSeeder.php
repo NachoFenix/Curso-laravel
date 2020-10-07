b4248793420f5bb1c9d295b9202cafdb724245a6
Ignacio <nacho.diaz.ray@gmail.com>
2020-10-07 11:52:52 -0500

    trabajo con seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Database\Events\StatementPrepared;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->truncateTable(['rol','permiso']);
+        $this->call(table_rol_seeder::class);
+        $this->call(TablaPermisoSeeder::class);
         // $this->call(UserSeeder::class);
+    }
+    protected function truncateTable(array $tablas){
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        foreach($tablas as $tabla){
+            DB::table($tabla)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
