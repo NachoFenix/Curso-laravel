2da4b9873cb0ea2e2e0bd065a4f93b34d2ecdc7a
Ignacio <nacho.diaz.ray@gmail.com>
2020-10-13 13:42:58 -0500

    se agregaron validacion de jquery

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -83,6 +83,9 @@
 <script src="{{asset("assets/lte/dist/js/adminlte.min.js")}}"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{asset("assets/lte/dist/js/demo.js")}}"></script>
+<script src="{{asset("assets/js/jquery-validation/jquery.validate.min.js")}}"></script>
+<script src="{{asset("assets/js/jquery-validation/localization/messages_es.min.js")}}"></script>
+<script src="{{asset("assets/js/funciones.js")}}"></script>
 @yield('scrips')
 </body>
 
