17a471bdfced778fbb1e7effba1fdd616fb83c92
Ignacio <nacho.diaz.ray@gmail.com>
2020-11-23 13:10:39 -0600

    Autenticacion

--- app/Http/Controllers/Seguridad/LoginController.php
+++ app/Http/Controllers/Seguridad/LoginController.php
@@ -2,28 +2,35 @@
 
 namespace App\Http\Controllers\Seguridad;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     use AuthenticatesUsers;
-    protected $redirecTo = '/admin';
+    protected $redirectTo = '/';
 
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
-    
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         return view('seguridad.index');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        $roles = $user->roles()->where('estado', 1)->get();
+        if ($roles->isNotEmpty()) {
+            $user->setSession($roles->toArray());
+        } else {
+            $this->guard()->logout();
+            $request->session()->invalidate();
+            return redirect('seguridad/login')->withErrors(['error' => 'Este usuario no tiene un rol activo']);
+        }
     }
 
     public function username()
--- database/seeds/UsuarioAdministradorSeeder.php
+++ database/seeds/UsuarioAdministradorSeeder.php
@@ -13,15 +13,27 @@ class UsuarioAdministradorSeeder extends Seeder
     public function run()
     {
         DB::table('usuario')->insert([
-            'usuario'=>'biblioteca_admin',
-            'password'=>bcrypt('pass123'),
-            'nombre'=>'admin'
-        ]);
-        DB::table('usuario_rol')->insert([
-            'rol_id'=>1,
-            'usuario_id'=>1,
-            'estado'=>1
+            'usuario' => 'biblioteca_admin',
+            'nombre' => 'Administrador',
+            'password' => bcrypt('pass123')
         ]);
 
+        DB::table('usuario')->insert([
+            'usuario' => 'rat',
+            'nombre' => 'Roosvelt',
+            'password' => bcrypt('pass123')
+        ]);
+
+        DB::table('usuario_rol')->insert([
+            'rol_id' => 1,
+            'usuario_id' => 1,
+            'estado' => 1
+        ]);
+
+        DB::table('usuario_rol')->insert([
+            'rol_id' => 2,
+            'usuario_id' => 2,
+            'estado' => 1
+        ]);
     }
 }
